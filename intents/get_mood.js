49ece5dff239c16fd3f6465b1a6e37829f8ffb85
Sade Sirén <sade@lingsoft.fi>
2018-07-30 09:57:00 +0300

    clean unneeded files

--- intents/get_mood.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'get_mood', // needs to match both the filename and the intent-name at Wit.ai
-    description: 'Asking about the mood of the bot',
-    execute(message, args) {
-        message.channel.send(`I'm not in the mood now.`);
-    },
-};
